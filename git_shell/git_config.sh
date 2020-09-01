7184366e9d06ea8a4ec356703241137a78a95610
Jovines <1246634075@qq.com>
2020-09-01 19:51:29 +0800

    添加git config脚本

---
+++ git_shell/git_config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# 给工作的老哥用的脚本，防止用公司的user.name来操作
+# 执行前配置一下，这里的用户名和邮箱，只会在这个项目生效
+user_name="Jovines"
+user_email="1246634075@qq.com"
+
+# 模式s：遍历当前文件夹以及所有文件夹
+# 默认模式：遍历当前目录子文件夹
+mode=$1
+
+file_count=0
+folder_count=0
+function FILE_COUNT(){
+  for file in *
+  do
+    if test -f "$file";then
+        ((file_count++))
+        echo "$file"
+    elif test -d "$file";then
+        ((folder_count++))
+        git config --local  user.name $user_name
+        git config --local user.email $user_email
+        git submodule foreach  git config --local  user.name $user_name
+        git submodule foreach  git config --local user.email $user_email
+        echo "$file"
+        if [ "$mode" == "s" ]; then
+            cd "$file"||return
+            (FILE_COUNT)
+            cd ..
+        fi
+    fi
+    done
+}
+
+FILE_COUNT
